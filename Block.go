0c88f9ea5b21a502b7ba90a7e560c48b9a2a04ec
Miles Benjamin <louren.of.courone@gmail.com>
2018-11-15 15:41:20 -0500

    Did some refactors.  Splitting out network code.
    Also fixed a few errors where our code is different from the tutorials.

--- Block.go
+++ Block.go
@@ -1,44 +1,24 @@
 package CoiNR
 
 import (
-	"bufio"
-	"context"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
-	"flag"
 	"fmt"
-	"io"
-	"log"
-	mrand "math/rand"
-	"net"
-	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
-
-	"github.com/davecgh/go-spew/spew"
-	golog "github.com/ipfs/go-log"
-	libp2p "github.com/libp2p/go-libp2p"
-	crypto "github.com/libp2p/go-libp2p-crypto"
-	host "github.com/libp2p/go-libp2p-host"
-	net "github.com/libp2p/go-libp2p-net"
-	peer "github.com/libp2p/go-libp2p-peer"
-	pstore "github.com/libp2p/go-libp2p-peerstore"
-	ma "github.com/multiformats/go-multiaddr"
-	gologging "github.com/whyrusleeping/go-logging"
 )
 
 // Block struct
 
 type Block struct {
-	Index        int
-	Timestamp    string
-	BPM          int
-	Hash         string
-	PreviousHash string
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+	Difficulty int
+	Nonce	string
 }
 
 // validates the block.  Usage is block.validate()
@@ -49,11 +29,11 @@ func (b *Block) validate(previous *Block) bool {
 		return false
 	}
 
-	if previous.Hash != b.PreviousHash {
+	if previous.Hash != b.PrevHash {
 		return false
 	}
 
-	if b.calculatehash() != b.Hash {
+	if b.calculateHash() != b.Hash {
 		return false
 	}
 
@@ -61,11 +41,11 @@ func (b *Block) validate(previous *Block) bool {
 
 }
 
-//calculates the hash for the block.  Usage is block.calculatehash()
+//calculates the hash for the block.  Usage is block.calculateHash()
 
-func (b *Block) calculatehash() string {
+func (b *Block) calculateHash() string {
 
-	hashString := strconv.Itoa(b.Index) + b.Timestamp + strconv.Itoa(b.BPM) + b.PreviousHash
+	hashString := strconv.Itoa(b.Index) + b.Timestamp + strconv.Itoa(b.BPM) + b.PrevHash
 
 	// I looked it up, this is one of two built-in hash functions in the go standard crypto lib.
 	// This one seems good.
@@ -90,8 +70,8 @@ func (b *Block) calculatehash() string {
 	newBlock.Index = prev.Index + 1
 	newBlock.Timestamp = t.String()
 	newBlock.BPM = BPM
-	newBlock.PreviousHash = prev.Hash
-	newBlock.Hash = newBlock.calculatehash()
+	newBlock.PrevHash = prev.Hash
+	newBlock.Hash = newBlock.calculateHash()
 
 	return newBlock
 
@@ -103,7 +83,7 @@ func (b *Block) calculatehash() string {
 //taken from https://medium.com/@mycoralhealth/code-your-own-blockchain-mining-algorithm-in-go-82c6a71aba1f
 //ill expand on this
 
-func generateBlock(oldBlock Block, BPM int) Block {
+func generateBlock(oldBlock Block, BPM int, difficulty int) Block {
 	var newBlock Block
 
 	t := time.Now()
@@ -115,15 +95,17 @@ func generateBlock(oldBlock Block, BPM int) Block {
 	newBlock.Difficulty = difficulty
 
 	for i := 0; ; i++ {
-		hex := fmt.Sprintf("%x", i)
-		newBlock.Nonce = hex
-		if !isHashValid(calculateHash(newBlock), newBlock.Difficulty) {
-			fmt.Println(calculateHash(newBlock), " do more work!")
+		hexVal := fmt.Sprintf("%x", i)
+		newBlock.Nonce = hexVal
+
+		hash := newBlock.calculateHash()
+		if !isHashValid(hash, newBlock.Difficulty) {
+			fmt.Println(hash, " do more work!")
 			time.Sleep(time.Second)
 			continue
 		} else {
-			fmt.Println(calculateHash(newBlock), " work done!")
-			newBlock.Hash = calculateHash(newBlock)
+			fmt.Println(hash, " work done!")
+			newBlock.Hash = hash
 			break
 		}
 
@@ -136,161 +118,6 @@ func isHashValid(hash string, difficulty int) bool {
 	return strings.HasPrefix(hash, prefix)
 }
 
-func makeNewPeer(listenPort int, secio bool, randseed int64) (host.Host, error) {
-	var rdr io.Reader
-
-	//not sure if we need, this check when we start running things
-	if randseed == 0 {
-		rdr = rand.Reader
-	} else {
-		rdr = mrand.New(mrand.NewSource(randseed))
-	}
-
-	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 1024, rdr)
-	if err != nil {
-		return nil, err
-	}
-
-	//get host names
-	var name1 string = "vdi-linux-030.ccs."
-	var name2 string = "vdi-linux-031.ccs."
-	addr1, err := net.LookupHost(name1)
-	addr2, err := net.LookupHost(name1)
-
-	//need to figure out how to set up hosts
-
-	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", listenPort)),
-		libp2p.Identity(priv),
-	}
-
-	basicHost, err := libp2p.New(context.Background(), opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	// Build host multiaddress
-	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", basicHost.ID().Pretty()))
-
-	// Now we can build a full multiaddress to reach this host
-	// by encapsulating both addresses:
-	addr := basicHost.Addrs()[0]
-	fullAddr := addr.Encapsulate(hostAddr)
-	log.Printf("I am %s\n", fullAddr)
-	if secio {
-		log.Printf("Now run \"go run main.go -l %d -d %s -secio\" on a different terminal\n", listenPort+1, fullAddr)
-	} else {
-		log.Printf("Now run \"go run main.go -l %d -d %s\" on a different terminal\n", listenPort+1, fullAddr)
-	}
-
-	return basicHost, nil
-
-}
-
-func handleStream(s net.Stream) {
-
-	log.Println("Got a new stream!")
-
-	// Create a buffer stream for non blocking read and write.
-	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
-
-	go readData(rw)
-	go writeData(rw)
-
-	// stream 's' will stay open until you close it (or the other side closes it).
-}
-
-func writeData(rw *bufio.ReadWriter) {
-
-	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-			mutex.Lock()
-			bytes, err := json.Marshal(Blockchain)
-			if err != nil {
-				log.Println(err)
-			}
-			mutex.Unlock()
-
-			mutex.Lock()
-			rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
-			rw.Flush()
-			mutex.Unlock()
-
-		}
-	}()
-
-	stdReader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("> ")
-		sendData, err := stdReader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		sendData = strings.Replace(sendData, "\n", "", -1)
-		bpm, err := strconv.Atoi(sendData)
-		if err != nil {
-			log.Fatal(err)
-		}
-		newBlock := generateBlock(Blockchain[len(Blockchain)-1], bpm)
-
-		if validate(newBlock, Blockchain[len(Blockchain)-1]) {
-			mutex.Lock()
-			Blockchain = append(Blockchain, newBlock)
-			mutex.Unlock()
-		}
-
-		bytes, err := json.Marshal(Blockchain)
-		if err != nil {
-			log.Println(err)
-		}
-
-		spew.Dump(Blockchain)
-
-		mutex.Lock()
-		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
-		rw.Flush()
-		mutex.Unlock()
-	}
-
-}
-
-func readData(rw *bufio.ReadWriter) {
-
-	for {
-		str, err := rw.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if str == "" {
-			return
-		}
-		if str != "\n" {
-
-			chain := make([]Block, 0)
-			if err := json.Unmarshal([]byte(str), &chain); err != nil {
-				log.Fatal(err)
-			}
-
-			mutex.Lock()
-			if len(chain) > len(Blockchain) {
-				Blockchain = chain
-				bytes, err := json.MarshalIndent(Blockchain, "", "  ")
-				if err != nil {
-
-					log.Fatal(err)
-				}
-				// Green console color: 	\x1b[32m
-				// Reset console color: 	\x1b[0m
-				fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
-			}
-			mutex.Unlock()
-		}
-	}
-}
 
 func proofOfWork() {
 
--- CoiNR.go
+++ CoiNR.go
@@ -6,6 +6,10 @@ import "sync"
 // This is the main class
 
 
+// Not sure this is where this should go. Storing the global difficulty of the blocks.
+var difficulty int
+
+
 // A block chain is just a slice of blocks
 var Blockchain []Block
 
